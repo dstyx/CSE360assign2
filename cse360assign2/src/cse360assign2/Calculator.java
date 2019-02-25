56ad9390b3cea4e4c53d7792e1a69be459ab9e0a
Dean Styx <dean.styx@yahoo.com>
2019-02-25 14:47:39 -0700

    Added Comments to the Calculator Class. Created a class to run and test calculator. Added a header to both classes.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,4 +1,12 @@
-
+/**Name: Dean Styx
+ * Class ID: 556
+ * Assignment 2
+ * 
+ * This Class is designed to act as a calculator that will add, subtract, multiply
+ * and divide numbers fed into each method respectively. This will all be worked on
+ * a total that is already stored and worked on. For example if the user does 2+2 then
+ * multiply 5, the total will be 20, not 2+2*5 = 12
+ */
 package cse360assign2;
 
 public class Calculator {
@@ -8,28 +16,42 @@ public class Calculator {
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
-	
+/**
+ * This function will return the current total.	
+ */
 	public int getTotal () {
 		return 0;
 	}
-	
+/**	
+ *This function will add numbers to the total
+ */
 	public void add (int value) {
 		
 	}
-	
+/**
+ * The subtract function will subract value from the total
+ */
 	public void subtract (int value) {
 		
 	}
-	
+/**
+ * Multiply will multiply the total by a value give.
+ */
 	public void multiply (int value) {
 		
 	}
-	
+/**
+ * Divide will divide the total by value unless the value is zero. If zero,
+ * the total will become zero
+ */
 	public void divide (int value) {
 		
 	}
-	
+/**
+ * The getHistory Function will be used to show the history for this iteration
+ * of calculator, including zero 
+ * @return
+ */
 	public String getHistory () {
 		return "";
 	}
---
+++ cse360assign2/src/cse360assign2/cse360assign2.java
@@ -0,0 +1,12 @@
+/**
+ * Name: Dean Styx
+ * Class ID: 556
+ * This class will be used to test and prove that Calculator is working correctly
+ * by running it though some tests.
+ */
+
+package cse360assign2;
+
+public class cse360assign2 {
+
+}
